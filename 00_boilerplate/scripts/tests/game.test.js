079a120526e86149bec8627b8fbd78326b6a1737
kedi1991 <okurut.henry@yahoo.com>
2023-04-14 22:24:59 +0000

    init 2

--- 00_boilerplate/scripts/tests/game.test.js
+++ 00_boilerplate/scripts/tests/game.test.js
@@ -1,3 +1,10 @@
 /**
  * @jest-environment jsdom
  */
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContent = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContent);
+    document.close();
+});
